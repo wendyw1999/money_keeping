b5535011d9cdcc86e8c05d3c976baa837db701ca
wendyw1999 <caw062@ucsd.edu>
2021-07-09 19:52:57 -0700

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React, { Component, useState, useEffect } from 'react'
+import React, { Component, useState, useEffect, useRef} from 'react'
 import { SketchPicker } from 'react-color'
 import { Input } from 'antd'
 import { DataStore } from '@aws-amplify/datastore'
 import { Item } from './models'
 import Button from 'react-bootstrap/Button';
-import { InputGroup,FormControl,Dropdown,DropdownButton,Card, Row,Col, Container } from 'react-bootstrap';
+import { InputGroup,FormControl,Dropdown,DropdownButton,Card, Row,Col, Container,
+Tab,Tabs,Toast,Popover,OverlayTrigger,Overlay,Tooltip,Modal } from 'react-bootstrap';
 
-const initialState = { color: '#000000', title:'', amount:'',category:'in'}
+const initialState = { color: '#000000', title:'', amount:'',category:'-'}
 
 function App () {
   const [formState, updateFormState] = useState(initialState)
   const [items, updateItems] = useState([])
   const [showPicker, updateShowPicker] = useState(false)
+  const [key, setKey] = useState('write');
+  const[showTooltip,setShowTooltip] = useState(false);
+  const[showModal,setShowModal] = useState(false);
+
+  const target = useRef(null);
+  var currItem = '';
+
+  
+function colorPickerButton() {
+  if (showPicker) {
+    return <i>隐藏颜色选择器</i>;
+  } else {return  <i>显示颜色选择器</i>}
+}
+  function handleCloseModal() {
+    setShowModal(false);
+  }
+  function handleShowModal() {
+    setShowModal(true);
+  }
+
+
 
   function toNumber(string) {
-    if (string == 'in') {
+    if (string == '+') {
       return 1
-    } else if (string == 'out') {
+    } else if (string == '-') {
       return -1
     } else {
       return 0
@@ -29,10 +51,10 @@ function App () {
     })
 
     function onChangeInOut(e) {
-      if (e.target.getAttribute('value') == 'out') {
-        updateFormState({...formState,category:'out'})
+      if (e.target.getAttribute('value') == '-') {
+        updateFormState({...formState,category:'-'})
       } else {
-        updateFormState({...formState,category:'in'})
+        updateFormState({...formState,category:'+'})
       }
     }
     function onChange(e) {
@@ -41,9 +63,10 @@ function App () {
       }
       else if (e.target.name == 'amount') {
           if (isNaN(e.target.value)) {
-            console.log(e.target.value)
+            setShowTooltip(true);
 
           } else {
+            setShowTooltip(false);
             const toNumeric = Number(e.target.value);
             
           updateFormState({...formState,amount:toNumeric})
@@ -70,6 +93,7 @@ function App () {
     async function deleteItem(item) {
       const todelete = await DataStore.query(Item,item.id);
       DataStore.delete(todelete);
+      setShowModal(false);
     }
     function processDate(dateString) {
       var d = new Date(dateString);
@@ -78,43 +102,82 @@ function App () {
 
       return dateToReturn
     }
+
+   function processToastHeader(item) {
+      const dateString = processDate(item.createdAt)
+      return <Toast.Header style = {{...itemStyle,color:item.color}}>
+            <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+            <strong className="mr-auto">{item.title}</strong>
+            <small>{dateString}</small>
+          </Toast.Header>
+
+    }
+    function handleModal(item) {
+
+      
+      return <Modal
+      show={showModal}
+      onHide={()=>handleCloseModal()}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>删除提醒:{item.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        确定删除该条记录？
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={()=>handleCloseModal()}>
+          取消
+        </Button>
+        <Button variant="danger" onClick = {() => deleteItem(item)}>确定删除</Button>
+      </Modal.Footer>
+    </Modal>
+    }
     return (
       <div style={container}>
         <h1 style = {heading}>
-          Money Keeping Book
+          记账本
         </h1>
-        
-        
-<InputGroup className="mb-3">
+        <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+    >
+      <Tab eventKey="write" title="记账">
+      
+      
+        <br></br>
+      <InputGroup className="mb-3">
 <DropdownButton
       as={InputGroup.Prepend}
       variant="outline-secondary"
       id="input-group-dropdown-1"
       title = {formState.category}
     >
-      <Dropdown.Item href="#" value = 'in' onClick = {onChangeInOut}>Money In +</Dropdown.Item>
-      <Dropdown.Item href="#" value = 'out' onClick = {onChangeInOut}>Money Out -</Dropdown.Item>
+      <Dropdown.Item href="#" value = '-' onClick = {onChangeInOut}>支出</Dropdown.Item>
+      <Dropdown.Item href="#" value = '+' onClick = {onChangeInOut}>收入</Dropdown.Item>
+
     </DropdownButton>
     <InputGroup.Prepend>
-      <InputGroup.Text>$</InputGroup.Text>
+      <InputGroup.Text>￥</InputGroup.Text>
     </InputGroup.Prepend>
-    <FormControl  
+    <FormControl  pattern="[0-9]*" ref={target}
     onChange = {onChange} id = 'amount' name = 'amount' placehodler = 'Amount' value = {formState.amount} 
     aria-label="Amount (to the nearest dollar)" />
-    <InputGroup.Append>
-      <InputGroup.Text>.00</InputGroup.Text>
-    </InputGroup.Append>
+    <Overlay target={target.current} show={showTooltip} placement="top">
+        {(props) => (
+          <Tooltip id="overlay-example" {...props}>
+            Please Enter Numbers
+          </Tooltip>
+        )}
+      </Overlay>
   </InputGroup>
-        {/* <Input onChange = {onChange} id = 'amount' name = 'amount' placehodler = 'Amount' value = {formState.amount}
-        style = {input}>
-        </Input> */}
-        {/* <Input onChange = {onChange} name = 'title' placehodler = 'Title' value = {formState.title}
-        style = {input}>
-        </Input> */}
   <InputGroup className="mb-3">
     <InputGroup.Prepend>
       <InputGroup.Text id="basic-addon3">
-        Description
+        描述
       </InputGroup.Text>
     </InputGroup.Prepend>
     <FormControl 
@@ -123,56 +186,52 @@ function App () {
   </InputGroup>
 
         <div>
-        <Button variant="light" onClick = {() => updateShowPicker(!showPicker)}>
-        Toggle Color Picker
+        <Button variant="outline-secondary" onClick = {() => updateShowPicker(!showPicker)}>
+        {colorPickerButton()}
       </Button>
-      <p>Color: <span style={{fontWeight:'bold', color:formState.color}}>{formState.color}</span></p>
+      <p>颜色: <span style={{fontWeight:'bold', color:formState.color}}>{formState.color}</span></p>
         </div>
         {
       showPicker && <SketchPicker color={formState.color} onChange={onChange}/>
 
     }
-    <Button variant='primary' onClick={createItem}>Create Item</Button>
-    <p>Running Total: ￥{items.reduce((a,v) =>  a = a + toNumber(v.category)*v.amount , 0 )}</p>
-    {
+    <Button variant='primary' onClick={createItem}>记账</Button>
+    <p>当前结余 ￥{items.reduce((a,v) =>  a = a + toNumber(v.category)*v.amount , 0 )}</p>
+      </Tab>
+      <Tab eventKey="history" title="历史记录">
+      <br></br>
+
+      {
 
       
-      items.map(item => (
-        <Card key = {item.id} style = {{...itemStyle,backgroundColor:item.color}}>
-            <Card.Body style = {itemBg}>
-              <Card.Title>{processDate(item.createdAt)}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted"> {item.title}</Card.Subtitle>
-
-              <Card.Text>
-               ￥{toNumber(item.category)*item.amount}
-                </Card.Text>
-
-            
-              <Button variant = 'danger' style = {button} onClick = {(e) => {
-            deleteItem(item)
-          }}>
-            Delete Item
-          </Button>
-            </Card.Body>
-          </Card>
-        // <div key ={item.id} style = {{...itemStyle,backgroundColor:item.color}}>
-        //   <div style={itemBg}>
-        //   <p style={itemAmount}>￥{toNumber(item.category)*item.amount},{item.title}</p>
-        //   <p>{item.createdAt}</p>
-          /* <form onSubmit = {(e) => {console.log("Do Something"); e.preventDefault();}}>
-          <Button variant = 'danger' style = {button} onClick = {(e) => {
-            deleteItem(item)
-          }}>
-            Delete Item
-          </Button>
-          </form> */
+        items.map(item => (
           
+
+          <Toast key = {item.id} onClose ={()=>handleShowModal()}>
+            {handleModal(item)}
+          {processToastHeader(item)}
+          <Toast.Body>￥{toNumber(item.category)*item.amount}</Toast.Body>
+          </Toast>
           
-        //   </div>
-        // </div>
+        ))
+        }
+      </Tab>
+      <Tab eventKey="stats" title="统计">
+        <Container>
+          <Row>
+          
+            <Col>收入</Col>
+            <Col>支出</Col>
+            <Col>结余</Col>
+          </Row>
+        </Container>
+      </Tab>
+    </Tabs>
+    
         
-      ))
-    } </div>
+        
+
+    </div>
       )
 }
 const dropDown = {marginBottom: 10,color:'black'}
